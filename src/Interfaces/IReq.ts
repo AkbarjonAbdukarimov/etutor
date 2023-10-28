b4e1a2fd49f4dfeb14f5da785989f416256fa8a3
AkbarjonAbdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-10-28 20:04:50 +0500

    fixed typos

--- src/Interfaces/IReq.ts
+++ src/Interfaces/IReq.ts
@@ -2,12 +2,15 @@ import { Request } from "express";
 import ILang from "./ILanguage";
 import IAdmin from "./IAdmin";
 import IUser from "./IUser";
+interface Iuser extends IUser {
+  code: string;
+}
 export default interface Req extends Request {
   // headers: {
   //   Authorization: string;
   // };
   body: {
-    user: IUser;
+    user: Iuser;
     admin: IAdmin;
     category: {
       name: ILang;
